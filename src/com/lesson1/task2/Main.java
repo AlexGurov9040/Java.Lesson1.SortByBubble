77e8fc90d0c3cbac07fbe83c9592ee066bc3a4c8
AlexGurov9040 <www.pers93@mail.ru>
2021-05-14 11:50:13 +0300

    add methods sortMas and displayMas

--- src/com/lesson1/task2/Main.java
+++ src/com/lesson1/task2/Main.java
@@ -5,11 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CompletionMas();
-    }
-
-    public static void CompletionMas(){
-
+        //filling in the array mas
         try{
             System.out.println("Введите количество элементов массива");
             Scanner in = new Scanner(System.in);
@@ -25,11 +21,14 @@ public class Main {
                     throw new Exception("Диапазон должен соответствовать правилу от < до");
                 }
                 int mas[] = new int[n];
-                System.out.println("Получен массив");
+                System.out.println("Исходный массив");
                 for (int i=0;i<n;i++){
                     mas[i] = a + (int)Math.round(Math.random()*(b-a));
                     System.out.print(mas[i]+" ");
                 }
+                System.out.println();
+                SortMas(mas);//sorting by the bubble method
+                displayMas(mas);//display an array mas
             }
             catch (Exception ex){
                 System.out.println(ex.getMessage());
@@ -37,6 +36,33 @@ public class Main {
         }
         catch (Exception ex){
             System.out.println(ex.getMessage());
+        }
+    }
+
+    public static int[] SortMas(int mas[]){
+
+        int i = 0;
+        boolean tr;
+        int el;
+        do{
+            tr = false;
+            for (int j=0;j< (mas.length-i-1);j++){
+                if (mas[j]>mas[j+1]){
+                    el = mas[j];
+                    mas[j] = mas[j+1];
+                    mas[j+1] = el;
+                    tr = true;
+                }
+            }
+            i++;
+        }
+        while (tr);
+        return mas;
+    }
+    public static void displayMas(int mas[]){
+        System.out.println("Получен массив");
+        for (int i=0;i<mas.length;i++){
+            System.out.print(mas[i]+" ");
         }
     }
 }
